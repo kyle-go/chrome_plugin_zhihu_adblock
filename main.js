add9142f43f602e489a68e5ff5505876a70ab514
sishenhei7 <982505136@qq.com>
2020-10-18 11:48:27 +0800

    refactor: delete jquery; add createFuckAd

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 (function () {
     console.log("【屏蔽知乎广告】插件加载成功!");
-    fuckTimelineAd();
 
-    // 屏蔽右侧广告（这个偶尔不管用，可能还没加载）
-    $(".Pc-card").css("display", "none");
-    console.log("已屏蔽右侧知乎广告");
+    // fuckAd
+    const createFuckAd = (adSelector, textSelector) => () => {
+        // 广告
+        const ads = document.querySelectorAll(adSelector);
+
+        if (ads.length > 0) {
+            const cardBrand = document.querySelector(textSelector);
+
+            if (cardBrand) {
+                console.log("已屏蔽广告:" + cardBrand.innerText);
+            }
+
+            // 删掉广告
+            [...ads].forEach(item => item.parentNode.removeChild(ads));
+        }
+    }
 
     // hook fetch
     const fetch_helper = {
@@ -13,39 +25,21 @@
             return fetch_helper.originalFetch(...args).then((response) => {
                 //console.log(response.url)
                 if (response.url.startsWith("https://www.zhihu.com/api/v3/feed/topstory/recommend?")) {
-                    setTimeout(fuckTimelineAd, 188);
+                    setTimeout(createFuckAd('.Pc-feedAd-container', '.Pc-feedAd-card-brand--bold'), 188);
                 }
                 if (response.url.startsWith("https://www.zhihu.com/api/v4/questions/")
                     && response.url.indexOf('answers?include') !== -1) {
-                    setTimeout(fuckAnswersAd, 288);
+                    setTimeout(createFuckAd('.Pc-word-card', '.Pc-word-card-brand-wrapper > span'), 288);
                 }
                 return response;
             });
         },
     }
+
     window.fetch = fetch_helper.myFetch;
+    window.onload = () => {
+        createFuckAd('.Pc-card')();
+        createFuckAd('.Pc-feedAd-container', '.Pc-feedAd-card-brand--bold')();
+        createFuckAd('.Pc-word-card', '.Pc-word-card-brand-wrapper > span')();
+    }
 })();
-
-function fuckTimelineAd() {
-    // 首页timeline广告
-    const ads = $(".Pc-feedAd-container");
-    if (ads.length > 0) {
-        const text = $(".Pc-feedAd-card-brand--bold").text();
-        if (text) {
-            console.log("已屏蔽广告:" + text);
-        }
-        ads.remove();
-    }
-}
-
-function fuckAnswersAd() {
-    // 答案列表广告
-    const ads = $(".Pc-word-card");
-    if (ads.length > 0) {
-        const text = $(".Pc-word-card-brand-wrapper > span").text();
-        if (text) {
-            console.log("已屏蔽广告:" + text);
-        }
-        ads.remove();
-    }
-}
